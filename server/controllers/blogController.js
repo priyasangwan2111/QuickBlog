6cfafd233e7fd4df35f4f61d2c302b141fbff570
priya <priya211@gmail.com>
2025-08-22 20:12:59 +0530

    api for blog delete update

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -52,6 +52,45 @@ export const getAllBlogs=async(req,res)=>{
     }
 }
 
-export const getBlogById=async(req,res)=<{
+export const getBlogById=async(req,res)=>{
+    try{
+    const{blogId}=req.params;
+    const blog =await Blog.findById(blogId)
     
+    if(!blog){
+        return res.json({success:false,message:"blog not found"});
+    }
+    res.json({success:true,blog})
 }
+    catch(error){
+        res.json({success:false,message:error.message})
+    }
+
+}
+export const deleteBlogById=async(req,res)=>{
+    try{
+    const{id}=req.body;
+    await Blog.findByIdAndDelete(id);
+    res.json({success:true,message:'blog deleted successfully'})
+    
+   
+}
+    catch(error){
+        res.json({success:false,message:error.message})
+    }
+
+}
+
+export const togglePublish=async(req,res)=>{
+    try{
+        const{id}=req.body;
+        const blog=await Blog.findById(id);
+        blog.isPublished=!blog.isPublished;
+        await blog.save();
+        res.json({success:true,message:'blog satus updated'})
+    }
+    catch(error){
+        res.json({success:false,message:error.message})
+    }
+}
+
--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -1,9 +1,13 @@
 import express from "express"
 
-import { addBlog } from "../controllers/blogController.js";
+import { addBlog, deleteBlogById, getAllBlogs,getBlogById, togglePublish } from "../controllers/blogController.js";
 import upload from "../middleware/multer.js";
 import auth from "../middleware/auth.js";
 
 const blogRouter=express.Router();
 blogRouter.post("/add",upload.single('image'),auth,addBlog)  //added middleware
+blogRouter.get('/all',getAllBlogs)
+blogRouter.get('/:blogId',getBlogById)
+blogRouter.post('/delete',auth,deleteBlogById)
+blogRouter.post('/toggle-publish',auth,togglePublish)
 export  default blogRouter
